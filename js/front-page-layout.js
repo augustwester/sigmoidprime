bee80d39f2bb19496b7dc71ea843ffa6484a9a3d
August Wester <august@Augusts-MacBook-Pro.local>
2017-04-07 19:20:12 +0200

    Add new design and new post

--- js/front-page-layout.js
+++ js/front-page-layout.js
@@ -1,18 +1,16 @@
 article_containers = document.getElementsByClassName("front-page-article-container");
 usragent_regex = new RegExp("Android|webOS|iPhone|iPad|iPod|BlackBerry");
 
-for (x in article_containers) {
-  if (x % 2) {
-    article_containers[x].style = "background-color:rgba(244,248,249,1);";
-  }
-
-  if (x != 0 && !usragent_regex.test(navigator.userAgent)) {
+if (!usragent_regex.test(navigator.userAgent)) {
+  for (x in article_containers) {
     article = article_containers[x].getElementsByTagName("article")[0];
-    reading_time = article.getElementsByClassName("reading-time")[0];
+    flexbox = article.getElementsByTagName("div")[0];
+    headline = article.getElementsByTagName("h1")[0];
+    excerpt = article.getElementsByTagName("p")[0];
 
-    reading_time_y = reading_time.offsetTop + reading_time.clientHeight;
-    container_height = article_containers[x].clientHeight;
-
-    article_containers[x].style.paddingTop = (container_height - reading_time_y) + "px";
+    alert(headline.clientHeight + excerpt.clientHeight);
+    //flexbox.style.height = article.offsetHeight + "px";
+    //thumbnail = article.getElementsByTagName("img")[0];
+    //thumbnail.style.marginTop = (article.offsetHeight - thumbnail.offsetHeight) / 2 + "px";
   }
 }
